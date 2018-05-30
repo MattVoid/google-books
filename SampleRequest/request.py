38097d7226eb2f7351b3ebc9b9ac8881bbcdfefb
Matteo Monteleone <am.monteleone.am@gmail.com>
2018-05-30 15:50:24 +0200

    Update request.py

--- SampleRequest/request.py
+++ SampleRequest/request.py
@@ -4,29 +4,27 @@ import sys
 import os
 
 try:
-    r = requests.get(url=sys.argv[1])
-    json_data = r.json()
+	r = requests.post(url='https://google-books-test.glitch.me/', data={'link' : sys.argv[1]})
+	json_data = r.json()
+	# takes links from the JSON
+	lista = json_data['list'][0]['pages']
+	title = json_data['list'][0]['title'][0]
+	# check if the book folder exists
+	try:
+		os.stat(title)
+	except:
+		os.mkdir(title)
 
-    # takes links from the JSON
-    lista = json_data['list'][0]['pages']
-
-    # check if the book folder exists
-    try:
-        os.stat("book")
-    except:
-        os.mkdir("book")
-
-    # downloads file
-    print("Download in progress...")
-    for i in range(len(lista)):
-        nome_pagina = lista[i].split("&pg=")[1].split("&img=")[0]
-
-        print("Download of " + nome_pagina)
-        r = requests.get(url=lista[i])
-        file = open("book/" + nome_pagina + ".png", "wb")
-        file.write(r.content)
-        file.close()
-    print("Done")
+	# downloads file
+	print("Download in progress...")
+	for i in range(len(lista)):
+		nome_pagina = lista[i].split("&pg=")[1].split("&img=")[0]
+		print("Download of " + nome_pagina)
+		r = requests.get(url=lista[i])
+		file = open( title + "/" + nome_pagina + ".png", "wb")
+		file.write(r.content)
+		file.close()
+	print("Done")
 
 except:
-    print("Error URL check README.md")
+	print("Error URL check README.md")
